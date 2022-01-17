73fa63cfc1d7b8d59c3021a0a2ffb19c3b0247cc
Yuki Nishimura <yukinkopen@gmail.com>
2022-01-17 23:30:14 +0900

    Lesson5 7.4までの実装を追加

---
+++ Assets/BrightnessRegulator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessRegulator : MonoBehaviour
+{
+    Material myMaterial;
+    private float minEmission = 0.3f;
+    private float magEmission = 2.0f;
+    private int degree = 0;
+    private int speed = 10;
+    Color defaultColor = Color.white;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (tag == "SmallStarTag")
+        {
+            this.defaultColor = Color.white;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.defaultColor = Color.yellow;
+        }
+        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.defaultColor = Color.cyan;
+        }
+        this.myMaterial = GetComponent<Renderer>().material;
+        myMaterial.SetColor("_EmissionColor", this.defaultColor * minEmission);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.degree >= 0)
+        {
+            Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin(this.degree * Mathf.Deg2Rad) * this.magEmission);
+            myMaterial.SetColor("_EmissionColor", emissionColor);
+            this.degree -= this.speed;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        this.degree = 180;
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFlipperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFlipperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFlipperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFlipperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+    }
+
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
