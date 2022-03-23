a421d5d28121cb7aae48d2b7485f71998b163fca
Yuki Nishimura <yukinkopen@gmail.com>
2022-03-23 19:38:35 +0900

    キー操作を追加
    下記の配置に修正
    左キーまたはAキー：左フリッパーを操作
    右キーまたはDキー：右フリッパーを操作
    Sキーまたは下キー：両方のフリッパーを操作

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -8,6 +8,7 @@ public class FripperController : MonoBehaviour
 
     private float defaultAngle = 20;
     private float flickAngle = -20;
+    private bool isLeftFlipperDown = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,19 +20,29 @@ public class FripperController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFlipperTag")
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && tag == "LeftFlipperTag")
         {
             SetAngle(this.flickAngle);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFlipperTag")
-        {
-            SetAngle(this.flickAngle);
-        }
-        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFlipperTag")
+        if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)) && tag == "LeftFlipperTag")
         {
             SetAngle(this.defaultAngle);
         }
-        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFlipperTag")
+
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && tag == "RightFlipperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if ((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D)) && tag == "RightFlipperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && (tag == "LeftFlipperTag" || tag == "RightFlipperTag"))
+        {
+            SetAngle(this.flickAngle);
+        }
+        if ((Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S)) && (tag == "LeftFlipperTag" || tag == "RightFlipperTag"))
         {
             SetAngle(this.defaultAngle);
         }
