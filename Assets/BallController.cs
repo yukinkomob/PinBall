23e75e039c9d959d58f54c0a9f07c849834bab66
Yuki Nishimura <yukinkopen@gmail.com>
2022-03-23 19:16:42 +0900

    得点の加算処理を追加

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -7,11 +7,13 @@ public class BallController : MonoBehaviour
 {
     private float visiblePosZ = -6.5f;
     private GameObject gameOverText;
+    private GameObject scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         this.gameOverText = GameObject.Find("GameOverText");
+        this.scoreText = GameObject.Find("ScoreText");
     }
 
     // Update is called once per frame
@@ -21,5 +23,33 @@ public class BallController : MonoBehaviour
         {
             this.gameOverText.GetComponent<Text>().text = "Game Over";
         }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        string objName = other.gameObject.name;
+        if (objName.StartsWith("LargeStar"))
+        {
+            addScore(20);
+        }
+        else if (objName.StartsWith("SmallStar"))
+        {
+            addScore(10);
+        }
+        else if (objName.StartsWith("LargeCloud"))
+        {
+            addScore(50);
+        }
+        else if (objName.StartsWith("SmallCloud"))
+        {
+            addScore(30);
+        }
+    }
+
+    void addScore(int score)
+    {
+        int currentScore = int.Parse(this.scoreText.GetComponent<Text>().text);
+        currentScore += score;
+        this.scoreText.GetComponent<Text>().text = currentScore.ToString();
     }
 }
